47df8ea678a78846b8517c81ecf86229e0ae9672
Jane <30421010+dracarystargaryen@users.noreply.github.com>
2021-01-17 18:34:17 -0500

    Add files via upload

---
+++ controlflow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+   int i = 5;
+
+   if(i == i == i) printf("Yes\n");
+
+   else printf("No\n");
+
+   
+
+   return 0;
+
+}//main
---
+++ loopsandimplementation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define MX 5
+
+int main() {
+
+   int i;
+
+  for(i = 1;i < MX;i++)
+
+     printf("i = %d\n",i);
+
+  return 0;
+
+}//main
